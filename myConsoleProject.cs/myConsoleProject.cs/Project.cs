7af6a3ab54859189aebfcc36580a64495ad0503c
Micah Gooden <micah734@me.com>
2022-02-15 18:12:05 -0500

    Add project files.

---
+++ myConsoleProject.cs/myConsoleProject.cs/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Project
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
