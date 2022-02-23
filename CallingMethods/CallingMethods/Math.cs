e8a9ac929bfe5f8cf15f15bd2d71c9c57f5f285a
Micah Gooden <micah734@me.com>
2022-02-23 15:58:40 -0500

    Math Methods

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Math
+    { 
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        
+       
+
+        
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number to do some math!");
+            int userInput=Convert.ToInt32(Console.ReadLine());
+            
+            
+           int addition=math.Add(userInput, 2);
+           int subtraction=math.Subtract(userInput, 2);
+           int multiplication=math.Multiply(userInput, 3);
+
+            Console.WriteLine(addition);
+            Console.WriteLine(subtraction);
+            Console.WriteLine(multiplication);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
