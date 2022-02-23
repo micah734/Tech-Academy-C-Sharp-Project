189e84771db256a7a6c5bcc7b5095c7c77ba997d
Micah Gooden <micah734@me.com>
2022-02-23 17:13:37 -0500

    Main Method Assignment added

---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            integer integer = new integer();
+
+            int division = integer.Divide(32, 4); //dividing by the two numbers
+            Console.WriteLine(division);
+
+           
+            decimal multiply = integer.Divide(33.12m,31.20m); //times by the numbers
+            Console.WriteLine(multiply);
+
+
+            int addition = integer.Divide("8", "12"); //converted to an integer, then added together
+            Console.WriteLine(addition);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/integer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class integer
+    {
+        public int Divide(int a, int b)
+        {
+            return a/b; //returned to the main program
+        }
+
+        public decimal Divide(decimal c, decimal d)//format is decimal
+        {
+            return c * d; //returned to the main program
+        }
+
+        public int Divide(string e, string f) //format is strings
+        {
+           int g=Convert.ToInt32(e); //converted to interval
+           int h= Convert.ToInt32(f); //converted to interval
+            return g + h;             //returned to the main program  
+
+        }
+    }
+}
