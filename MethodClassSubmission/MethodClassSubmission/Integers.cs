d3f6f38409dced295059262c7c20fd0a0ce09a9a
Micah Gooden <micah734@me.com>
2022-02-23 20:10:24 -0500

    Method Class Submission

---
+++ MethodClassSubmission/MethodClassSubmission/Integers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Integers
+    {
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("Number is: {0}",a);
+
+            Console.WriteLine(b * 4);
+
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integers integers = new Integers();
+
+            integers.sum(5, 12);
+
+            integers.sum(a: 13, b: 11);
+
+            Console.ReadLine();
+
+        }
+    }
+}
