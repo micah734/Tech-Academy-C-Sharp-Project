334498dd466e3e8061d4c861ff94a8435062aa0c
Micah Gooden <micah734@me.com>
2022-02-25 18:24:59 -0500

    Abstract Class Submission
    Using abstract classes and methods

---
+++ AbstractClassSubmission/AbstractClassSubmission/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public class Employee:Person //inheriting from Person class
+    {
+        public override void SayName() //defining and setting up the abstract method
+        {
+            Console.WriteLine(firstName + " " + lastName);
+            
+        }
+    }
+}
---
+++ AbstractClassSubmission/AbstractClassSubmission/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public abstract class Person //defining the abstract class
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+
+        public abstract void SayName(); //setting that this method must be included
+        
+    }
+}
