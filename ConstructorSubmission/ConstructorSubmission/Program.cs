ddca1395ca1b3c89cb5ebb2e21bb3b948ca4309c
Micah Gooden <micah734@me.com>
2022-03-08 17:04:19 -0500

    Constructor Updated

--- ConstructorSubmission/ConstructorSubmission/Program.cs
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -6,20 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConstructorSubmission
 {
-    class Program
+    public class constructor
     {
-        public string userName { get; set; }
-        public int idNumber {get; set;}
-        public Program(string userName, int idNumber)
+        public constructor()
         {
-            this.userName = userName; //stringing constructors together.
-            this.idNumber = idNumber;
+            Console.WriteLine("This Constructor has no parameters");
+
         }
+        public constructor(int x) : this()
+        {
+            Console.WriteLine("Constructor 2, value: {0}", x);
+        }
+        public constructor(int x, int y) : this(x)
+        {
+            Console.WriteLine("Constructor 3, value1: {0} & value2:{1}", x, y);
+        }
+    }
+
+    public class Program
+    {
+
         static void Main(string[] args)
 
-           
+
         {
-            
+        constructor constructor = new constructor(9, 43);
+         
         const string Name = "Micah Gooden";//assigning a constant of Micah Gooden for the Name variable
 
         var cost = 123.0m; //using var to create a variable that is not strongly typed and figured out by the program
