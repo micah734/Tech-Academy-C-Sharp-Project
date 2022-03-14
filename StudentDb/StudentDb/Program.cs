cbd919c8f546b48fc5857ef5d028151e03a4e0dd
Micah Gooden <micah734@me.com>
2022-03-14 17:48:36 -0400

    StudentDb Upload

---
+++ StudentDb/StudentDb/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StudentDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentFName = "Bob", StudentLName = "Jobs", Grade = 8 };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentFName { get; set; }
+        public string StudentLName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int Grade { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
